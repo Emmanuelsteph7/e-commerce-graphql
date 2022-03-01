aaccb1352244adb0da52944bc014f4af70f1db71
Emmanuelsteph7 <stephenemmanuel770@gmail.com>
2022-03-01 10:45:40 +0100

    feat: added products and product type definitions and resolvers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { products } = require("./data/index");
 
 // this defines how our query will look (schema)
 const typeDefs = gql`
   type Query {
     hello: String
+    products: [Product!]!
+    product(id: ID!): Product
+  }
+
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    image: String!
+    price: Float!
+    onSale: Boolean!
   }
 `;
 
 // this defines the resolvers. they are functions that simply return the data based on the particular query
 const resolvers = {
   Query: {
-    hello: () => {
-      return "Hello World";
+    hello: () => "John",
+    products: () => products,
+    product: (parent, args, context) => {
+      const productId = args.id;
+
+      const product = products.find((product) => product.id === productId);
+
+      if (!product) return null;
+
+      return product;
     },
   },
 };
@@ -21,7 +41,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen().then(({ url, ...args }) => {
-  console.log(args);
+server.listen().then(({ url }) => {
   console.log(`server is running at ${url}`);
 });
